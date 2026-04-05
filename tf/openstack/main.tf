d8e4b7e8c1cd5a8188813ecfff6d4e98e71c4843
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2026-04-05 19:30:08 +0700

    chore: update k8s manifests and refactor openstack terraform
    Kubernetes:
    - Disable mongodb, velero, and vaultwarden in ArgoCD app-of-app values.
    - Scale down PostgreSQL cluster instances from 3 to 2.
    - Scale down MongoDB replica set members from 3 to 2.
    - Increase CPU limits for mongodb-org-server and mongodb-agent containers (100m -> 500m) to ensure better performance.
    
    Terraform (OpenStack):
    - Add `.terraform.lock.hcl` to lock the OpenStack provider version.
    - Update `karina` subnet CIDR block to 192.168.200.0/24.
    - Upgrade the compute instance flavor to `gp.xlarge-8c` and configure it to boot from a 50GB block device volume rather than directly from the image.
    - Rename generic resource names to be more descriptive (e.g., `secgroup_rule_1` -> `secgroup_rule_ssh`).
    - Fix compute instance and networking port definitions to correctly reference the network.
    - Add output variables for the floating IP and a copy-pasteable SSH command.

--- tf/openstack/main.tf
+++ tf/openstack/main.tf
@@ -1,14 +1,14 @@
 resource "openstack_networking_network_v2" "karina" {
   name           = "karina"
-  admin_state_up = "true"
+  admin_state_up = true
 }
 
 resource "openstack_networking_subnet_v2" "karina" {
-  name       = "karina"
-  network_id = openstack_networking_network_v2.karina.id
-  cidr       = "192.168.199.0/24"
-  ip_version = 4
-  dns_nameservers = ["1.1.1.1","8.8.8.8"]
+  name            = "karina"
+  network_id      = openstack_networking_network_v2.karina.id
+  cidr            = "192.168.200.0/24"
+  ip_version      = 4
+  dns_nameservers = ["1.1.1.1", "8.8.8.8"]
 }
 
 resource "openstack_networking_secgroup_v2" "secgroup_1" {
@@ -16,7 +16,7 @@ resource "openstack_networking_secgroup_v2" "secgroup_1" {
   description = "a security group"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_1" {
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ssh" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
@@ -25,11 +25,11 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_1" {
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.secgroup_1.id
 }
+
 resource "openstack_compute_keypair_v2" "karina" {
   name       = "karina"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD1mqJ9VFW25ZILkw2pDbnFaa59O0pBIEDbT/V9R/HZT53Jgq+MNnS1pUAyIXEpu6Rf+HqiqhXtnwlnw2dLhL3VYiGFNA/ZxK7Jgemr1uQqgYNqtNQbFz4LBsN3/oR2gW4wFCUqS0CqA25xV30RhO0VRZEf1Ks+unCwbY8xQUsjTPYC5ief7OmGmP/fouTn84M04njptzRHfeJ1YAmm5PX1hewyaZN3zhW8R/6kHvqLg492/ywJ3Fh3qZk8Qu2STzPwfDRRzwuGkKEuO6itl9awe6uWE8gFGZoUsIQt3hjU2GCmLCvj2My+HCUBH1yz/aUo5iQuTbQFdDlwOIRppa+4ZsawHUSZnBrgzXjn3HGYb6XGNuWBzjXm2+XHlomex/5I3/sxCjVoME/IYgZxoGVPeJZSQ6cOYjU6PYK3+ygkbgWqVYrkBC0BJGruoAsDo5SUv27ShxIs7vZple+U3AhthGiCbjvSIv5GwpJwnYcHDcH46N35mTJNkweQzEHrqQ8= ngovu@LAPTOP-9NBAONU6"
 }
-
 
 resource "openstack_networking_router_v2" "karina" {
   name                = "karina"
@@ -37,29 +37,56 @@ resource "openstack_networking_router_v2" "karina" {
   external_network_id = "c3455e8f-ea16-4f5d-ad5e-5c4292015a0d"
 }
 
-resource "openstack_networking_router_interface_v2" "router_interface_1" {
+resource "openstack_networking_router_interface_v2" "karina" {
   router_id = openstack_networking_router_v2.karina.id
   subnet_id = openstack_networking_subnet_v2.karina.id
 }
 
 
+data "openstack_images_image_v2" "ubuntu" {
+  name        = "Ubuntu 22.04"
+  most_recent = true
+}
+
+
 resource "openstack_compute_instance_v2" "karina" {
   name            = "karina"
-  image_name      = "Ubuntu 22.04"
-  flavor_name     = "d30.l4"
+  flavor_name     = "gp.xlarge-8c"   # 8 vCPUs, 16 GB RAM
   key_pair        = openstack_compute_keypair_v2.karina.name
   security_groups = [openstack_networking_secgroup_v2.secgroup_1.name]
+  block_device {
+    uuid                  = data.openstack_images_image_v2.ubuntu.id
+    source_type           = "image"
+    destination_type      = "volume"
+    volume_size           = 50
+    boot_index            = 0
+    delete_on_termination = true
+  }
   network {
-    name = openstack_networking_subnet_v2.karina.name
+    name = openstack_networking_network_v2.karina.name
   }
 }
-data "openstack_networking_port_v2" "karina" {
-  device_id = openstack_compute_instance_v2.karina.id
-}
+
 resource "openstack_networking_floatingip_v2" "karina" {
   pool = "Public_Net"
 }
+
+data "openstack_networking_port_v2" "karina" {
+  device_id  = openstack_compute_instance_v2.karina.id
+  network_id = openstack_networking_network_v2.karina.id
+}
+
 resource "openstack_networking_floatingip_associate_v2" "karina" {
   floating_ip = openstack_networking_floatingip_v2.karina.address
   port_id     = data.openstack_networking_port_v2.karina.id
+}
+
+output "instance_floating_ip" {
+  value       = openstack_networking_floatingip_v2.karina.address
+  description = "Public IP to SSH into the instance"
+}
+
+output "ssh_command" {
+  value       = "ssh -i <your-private-key> ubuntu@${openstack_networking_floatingip_v2.karina.address}"
+  description = "SSH command"
 }
