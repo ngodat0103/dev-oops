dd172f7499e394b587fb1b95060cb6ede7fc2285
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2026-04-14 21:07:57 +0700

    refactor(infra): consolidate pg databases and migration of juicefs
    - Consolidate individual CloudNativePG Database manifests into a single `databases.yaml`.
    - Migrate JuiceFS database and user from Neon (Terraform) to local PostgreSQL (CNPG).
    - Remove unused SMB CSI Driver and MetalLB ArgoCD applications.
    - Cleanup unused Neon roles and database resources.

--- tf/neon/main.tf
+++ tf/neon/main.tf
@@ -13,19 +13,6 @@ resource "neon_project" "production" {
   history_retention_seconds = 21600
 }
 
-resource "neon_role" "juicefs" {
-  project_id = neon_project.metadata.id
-  branch_id  = neon_project.metadata.default_branch_id
-  name       = "juicefs"
-}
-resource "neon_database" "juicefs" {
-  project_id = neon_project.metadata.id
-  branch_id  = neon_project.metadata.default_branch_id
-  name       = "juicefs"
-  owner_name = neon_role.juicefs.name
-}
-
-
 resource "neon_role" "vaultwarden" {
   project_id = neon_project.production.id
   branch_id  = neon_project.production.default_branch_id
