3feda16b431c675b8c30e1b17b3ac2bf543dcfef
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2026-08-02 13:52:11 +0700

    chore: replace kibana dns with qbittorrent and update juicefs secret

--- tf/cloudflare/dns/main.tf
+++ tf/cloudflare/dns/main.tf
@@ -72,7 +72,7 @@ module "ddns_records" {
       content = local.traefik_k8s_lan_ip
       ttl     = 1
     }
-    kibana = {
+    qbittorrent = {
       type    = "A"
       proxied = false
       content = local.traefik_k8s_lan_ip
